d3d5e70430b8423ddd0a84d62db328317b92fb21
Tesoem <Tesoem@ISC-Tesoem-iMac.local>
2019-03-28 11:10:14 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/luis/itics/barradedezplazamiento/Barras.java
@@ -0,0 +1,58 @@
+package mx.edu.tesoem.luis.itics.barradedezplazamiento;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Barras extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barras);
+    }
+
+
+
+    Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barra_desplazamiento);
+
+        <TextView
+                android;layout.heigh"match-content"
+        android;_height"391dp"/>
+
+                <LinerLayout
+                android;layout.heigh"match-content"
+        android;_height"wrap-parent"
+        android;orientation"vertical"/>
+
+                <EditText
+                android;layout.heigh"match-content"
+        android;_height"wrap-parent"
+        android;text"PrimerTexto"/>
+
+                <button
+                android;layout.heigh"match-content"
+        android;_height"wrap-parent"
+        android;Button"PrimerBoton"/>
+
+                <TextView
+                android;layout.heigh"match-content"
+        android;_height"wrap-parent"
+        android;Text"SegundoTexto"/>
+
+                <button
+                android;layout.heigh"match-content"
+        android;_height"wrap-parent"
+        android;Button"PrimerBoton"/>
+
+                </LinearLayout>
+                </ScrollView>
+                <Button
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        android:text=button final"/>
+
+                </LinearLayout>
+}
